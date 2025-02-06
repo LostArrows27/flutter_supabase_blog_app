15532699e5baade0ef9d6d02934d54c9923a760d
LostArrows27 <thelastofus2isnuts@gmail.com>
2025-02-06 15:50:44 +0700

    feat: sign up usecase + install bloc dev

---
+++ lib/features/auth/domain/usecases/user_signup.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_supabase/core/error/failure.dart';
+import 'package:flutter_supabase/core/usecase/usecase.dart';
+import 'package:flutter_supabase/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UserSignup implements UseCase<String, UserSignupParams> {
+  final AuthRepository authRepository;
+
+  const UserSignup(this.authRepository);
+
+  @override
+  Future<Either<Failure, String>> call(UserSignupParams params) async {
+    return await authRepository.signUpWithEmailPassword(
+        name: params.name, email: params.email, password: params.password);
+  }
+}
+
+class UserSignupParams {
+  final String name;
+  final String email;
+  final String password;
+
+  UserSignupParams(
+      {required this.name, required this.email, required this.password});
+}
