e532377d2ebf7e75c91d723ca9ab68927a46cc68
LostArrows27 <thelastofus2isnuts@gmail.com>
2025-02-06 11:14:35 +0700

    feat: sign up data layer

---
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_supabase/core/error/failure.dart';
+import 'package:flutter_supabase/core/error/server_exception.dart';
+import 'package:flutter_supabase/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:flutter_supabase/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+
+  const AuthRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<Either<Failure, String>> loginWithEmailPassword(
+      {required String email, required String password}) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword(
+      {required String name,
+      required String email,
+      required String password}) async {
+    try {
+      final userId = await remoteDataSource.signUpWithEmailAndPassword(
+          name: name, email: email, password: password);
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -4,13 +4,13 @@ import 'package:flutter_supabase/core/error/failure.dart';
 import 'package:fpdart/fpdart.dart';
 
 abstract interface class AuthRepository {
-  Either<Failure, String> signUpWithEmailPassword({
+  Future<Either<Failure, String>> signUpWithEmailPassword({
     required String name,
     required String email,
     required String password,
   });
 
-  Either<Failure, String> loginWithEmailPassword({
+  Future<Either<Failure, String>> loginWithEmailPassword({
     required String email,
     required String password,
   });
